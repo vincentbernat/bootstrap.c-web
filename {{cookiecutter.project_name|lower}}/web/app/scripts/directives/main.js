ac9ac986af73b7422b10a42fb7eff85bf8d908ce
Vincent Bernat <vincent@bernat.im>
2014-05-26 20:54:09 +0200

    angular: fix module declaration

--- {{cookiecutter.project_name|lower}}/web/app/scripts/directives/main.js
+++ {{cookiecutter.project_name|lower}}/web/app/scripts/directives/main.js
@@ -1,5 +1,5 @@
 (function() {
   'use strict';
 
-  angular.module('{{cookiecutter.small_prefix}}controller.directives');
+  angular.module('{{cookiecutter.small_prefix}}controller.directives', []);
 })();
